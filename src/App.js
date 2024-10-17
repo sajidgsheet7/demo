0b867e7d793eb2eef569e770b7c50be206c3e6ee
sameer <sajidgsheet39@gamil.com>
2024-10-17 15:55:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import RecordManager from './RecordManager';
+
+function App() {
+    return (
+        <div className="App">
+            <RecordManager />
+        </div>
+    );
+}
+
+export default App;
